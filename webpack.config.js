e561050b2cdafe9efc89df1971a934e90e1e3f84
Hrithik Biswas <mr.hrithikbiswas@gmail.com>
2025-06-09 03:08:21 +0600

    feat: add main movie poster section with "Up next" trailers sidebar to homepage layout

---
+++ webpack.config.js
@@ -0,0 +1,100 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const config = {
+    entry: {
+        main: './src/assets/js/index.js',
+        tailwind: './src/assets/css/tailwind.css',
+        fontAwesome: './src/assets/js/font-awesome.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: './assets/js/[name].bundle.js',
+        assetModuleFilename: './assets/[base]', // or => assetModuleFilename: './assets/uploads/[name][ext]'
+        clean: true,
+    },
+    mode: 'development',
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'src'),
+        },
+        port: 8080,
+        open: true,
+        compress: true,
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+                // options: {
+                //     minimize: true, // html minify method 01
+                // },
+            },
+            {
+                test: /\.css$/,
+                // include: path.resolve(__dirname, 'src'),
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/img/[name][ext]',
+                },
+            },
+            {
+                test: /\.(mp4|webm|wmv)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/video/[name][ext]',
+                },
+            },
+            {
+                test: /\.(ttf|woff|woff2)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/fonts/[name][ext]',
+                },
+            },
+        ],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin({
+                terserOptions: {
+                    format: {
+                        comments: false,
+                    },
+                },
+                extractComments: false,
+            }),
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            inject: 'body',
+            minify: {
+                collapseWhitespace: true, // html minify method 02
+                removeComments: true,
+            },
+        }),
+        new MiniCssExtractPlugin({
+            filename: './assets/css/[name].bundle.css',
+            chunkFilename: './css/[id].css',
+        }),
+    ],
+};
+
+module.exports = config;
